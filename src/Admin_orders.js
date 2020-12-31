87750a5c13aee9584f00b78d8f7c537a7c2587c1
Matt444 <joniec.mateusz44@gmail.com>
2020-12-31 16:30:52 +0100

    admin/orders structure improvements

--- src/Admin_orders.js
+++ src/Admin_orders.js
@@ -1,49 +1,39 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { TableGames } from './components/TableGames';
 import { LayoutAdmin } from './components/LayoutAdmin';
+
+const orders = [
+    {
+        id: '2',
+        timedate: '22:26 18.12.2020',
+        username: 'Andrzej',
+        games: [
+            {name: 'Cyberpunk 2077 - PC | KLUCZ - PCXWA-2TH6M-3H53W', price: '200 zł'},
+            {name: 'Wiedźmin 3 - PS4 | BOX', price: '300 zł'}
+        ]
+    },
+    {
+        id: '1',
+        timedate: '10:15 11.12.2020',
+        username: 'Andrzej',
+        games: [
+            {name: 'Cyberpunk 2077 - PC | KLUCZ - PCXWA-2TH6M-3H53W', price: '200 zł'},
+            {name: 'Wiedźmin 3 - PS4 | BOX', price: '300 zł'}
+        ]
+    }
+]
+
+const AdminOrder = (props) => {
+    return (
+        <div>
+            <p className="fltr">#{ props.order.id } - { props.order.timedate } - { props.order.username }</p>
+            <TableGames games={props.order.games} />
+        </div>  
+    );
+}
 
 export const Admin_orders = () => (
     <LayoutAdmin>
-        <p className="fltr">#2 - 22:26 18.12.2020 - Andrzej</p>
-        <Table responsive>
-            <thead>
-                <tr>
-                    <th className="no-border-top">Gra</th>
-                    <th className="no-border-top">Cena</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Cyberpunk 2077 - PC | KLUCZ STEAM - PCXWA-2TH6M-3H53W</td>
-                    <td>200 zł</td>
-                </tr>
-                <tr>
-                    <td>Wiedźmin 3 - PS4 | BOX</td>
-                    <td>300 zł</td>
-                </tr>
-                <tr><td colspan="2"></td></tr>
-            </tbody>
-        </Table>
-
-        <p className="fltr">#1 - 10:15 11.12.2020 - Andrzej</p>
-        <Table responsive>
-            <thead>
-                <tr>
-                    <th className="no-border-top">Gra</th>
-                    <th className="no-border-top">Cena</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Cyberpunk 2077 - PC | KLUCZ STEAM - PCXWA-2TH6M-3H53W</td>
-                    <td>200 zł</td>
-                </tr>
-                <tr>
-                    <td>Wiedźmin 3 - PS4 | BOX</td>
-                    <td>300 zł</td>
-                </tr>
-                <tr><td colspan="2"></td></tr>
-            </tbody>
-        </Table>
+        { orders.map( (order) => <AdminOrder order={order} /> ) }
     </LayoutAdmin>
 );
