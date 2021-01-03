142f37a65821782d082540b70e498294647a3d05
Matt444 <joniec.mateusz44@gmail.com>
2021-01-03 20:32:03 +0100

    added myaccount/edit form validation

--- src/User_edit.js
+++ src/User_edit.js
@@ -1,45 +1,91 @@
 import React from 'react';
 import { Button, Form, Row, Col } from 'react-bootstrap';
 import { LayoutMyAccount } from './components/LayoutMyAccount';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const schema = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().min(2).max(15).required(),
+    password2: yup.string().min(2).max(15).required(),
+});
 
 export const User_edit = () => (
     <LayoutMyAccount>
         <p className="fltr">Zmień email</p>
 
-        <Form className="mb-4">
-            <Row>
-                <Col>
-                    <Form.Control type="email" placeholder="Wpisz nowy email" />
-                </Col>
-                <Col>
-                    <Button variant="dark" type="submit">
-                        Zastosuj
-                    </Button>
-                </Col>
-            </Row>
-        </Form>
+        <Formik 
+            initialValues={{
+                email: '',
+            }}
+            onSubmit={values => {                
+                console.log(values);
+            }}
+            validationSchema={schema}
+        >
+            {({ handleSubmit, handleChange, values, isValid, errors, touched }) => (
+
+                <Form className="mb-4" onSubmit={handleSubmit} noValidate>
+                    <Row>
+                        <Col>
+                            <Form.Control name="email" type="email" value={values.email} onChange={handleChange}
+                                isInvalid={touched.email && !!errors.email} isValid={touched.email && !errors.email} placeholder="Wpisz nowy email" />
+                            <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                        </Col>
+                        <Col>
+                            <Button variant="dark" type="submit">
+                                Zastosuj
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+
+            )}
+            
+
+        </Formik>
 
         <p className="fltr">Zmień hasło</p>
 
-        <Form>
-            <Row className="mb-3">
-                <Col>
-                    <Form.Control type="password" placeholder="Wpisz nowe hasło" />
-                </Col>
-                <Col>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Form.Control type="password" placeholder="Wpisz ponownie nowe hasło" />
-                </Col>
-                <Col>
-                    <Button variant="dark" type="submit">
-                        Zastosuj
-                    </Button>
-                </Col>
-            </Row>
-        </Form>
+        <Formik 
+            initialValues={{
+                password: '',
+                password2: ''
+            }}
+            onSubmit={values => {
+                console.log(values);
+            }}
+            validationSchema={schema}
+        >
+
+        {({ handleSubmit, handleChange, values, isValid, errors, touched }) => (
+            <Form onSubmit={handleSubmit} noValidate>
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Control name="password" type="password" value={values.password} onChange={handleChange}
+                            isInvalid={touched.password && !!errors.password} isValid={touched.password && !errors.password} 
+                            placeholder="Wpisz nowe hasło" />
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Control name="password2" type="password" value={values.password2} onChange={handleChange}
+                            isInvalid={touched.password2 && !!errors.password2} isValid={touched.password2 && !errors.password2} 
+                            placeholder="Powtórz nowe hasło" />
+                    </Col>
+                    <Col>
+                        <Button variant="dark" type="submit">
+                            Zastosuj
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
+
+            )}
+
+        </Formik>
 
     </LayoutMyAccount>
 );
