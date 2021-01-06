d4e58b9ddf3f58e042d0e58424fe6b295b0e6346
Matt444 <joniec.mateusz44@gmail.com>
2021-01-07 00:50:30 +0100

    visual correction

--- src/Forbidden.js
+++ src/Forbidden.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export const Forbidden = () => (
-    <div>
-        <h1>403 Forbidden</h1>
+    <div className="mt-5">
+        <h1 className="font-weight-bold">403 Forbidden</h1>
         <h2>Sorry, you have no power here ;)</h2>
+        <Button href="/login" className="p-0 mb-1" variant="link" >Login</Button>
+        <span className="fbbt"> or </span>
+        <Button href="/" className="p-0 mb-1" variant="link">back to home</Button>
     </div>
 );
