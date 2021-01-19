9121015b6b6c6f5a0d3cc36e2cfa89eb5fc58129
Matt444 <joniec.mateusz44@gmail.com>
2021-01-19 16:54:16 +0100

    add games in admin panel

--- src/Admin_games.js
+++ src/Admin_games.js
@@ -5,13 +5,43 @@ import { LayoutAdmin } from './components/LayoutAdmin';
 import { BoxArrowUpRight, PencilSquare, XCircle } from 'react-bootstrap-icons';
 import DropdownMultiselect from 'react-multiselect-dropdown-bootstrap';
 import DatePicker from 'react-date-picker';
-import {array} from "yup/lib/locale";
-import toArray from "yup/lib/util/toArray";
+import { Formik, useField, useFormikContext } from 'formik';
+import * as yup from 'yup';
 
+const DatePickerField = ({ ...props }) => {
+    const { setFieldValue } = useFormikContext();
+    const [field] = useField(props);
+    return (
+      <DatePicker
+        {...field}
+        {...props}
+        selected={(field.value && new Date(field.value)) || null}
+        onChange={val => {
+          setFieldValue(field.name, val);
+        }}
+      />
+    );
+  };
 
+const DropdownMultiselectField = ({ ...props }) => {
+    const { setFieldValue } = useFormikContext();
+    const [field] = useField(props);
+    return (
+      <DropdownMultiselect
+        {...field}
+        {...props}
+        
+        handleOnChange={val => {
+          setFieldValue(field.name, val);
+        }}
+      />
+    );
+  };
 
+  
 export const Admin_games = (props) => {
     const [value, onChange] = useState(new Date());
+    const [allGames, setAllGames] = useState([]);
     const [name, setName] = useState("");
     const [price, setPrice] = useState(0);
     const [quantity, setQuantity] = useState(0);
@@ -19,17 +49,39 @@ export const Admin_games = (props) => {
     const [categories, setCategories] = useState([]);
     const [age, setAge] = useState();
     const [platform, setPlatform] = useState();
-    const [allPlatforms, setAllPlatforms] = useState();
-    const [allCategories, setAllCategories] = useState();
+    const [allPlatforms, setAllPlatforms] = useState([]);
+    const [allCategories, setAllCategories] = useState([]);
+    const [gameAdded, setGameAdded] = useState(false);
     const [adding, setAdding] = useState(true);
-    var handleRender;
-    useEffect(() => {
-        fetch("/platforms").then(response =>
-            response.json().then(data => {
-                setAllPlatforms(data.platforms);
-            })
-        );
-    }, []);
+    
+
+    const updateGames = async () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        let i = 1;
+        let totalPages = 1;
+        let arr = [];
+
+        while (i <= totalPages) {
+            const res = await fetch("/games", {
+                method: 'POST',
+                headers: myHeaders,
+                body: JSON.stringify({"search_filter":{
+                    "digital": 1,
+                    "page_number": i
+                }}),
+                redirect: 'follow'
+            });
+            const data = await res.json();
+            totalPages = Math.ceil(data.total_number / data.results_per_page);
+            
+            arr = await arr.concat(data.games);
+            i++;
+        }
+
+        setAllGames(arr);
+    }
 
     useEffect(() => {
         fetch("/categories").then(response =>
@@ -37,34 +89,17 @@ export const Admin_games = (props) => {
                 setAllCategories(data.categories);
             })
         );
+
+        fetch("/platforms").then(response =>
+            response.json().then(data => {
+                setAllPlatforms(data.platforms);
+            })
+        );
+
+        updateGames();
+
     }, []);
 
-    let platformsItems;
-    if (allPlatforms){
-        platformsItems = allPlatforms.map(item => {
-            return(
-                <Dropdown.Item onClick={e => {
-                    e.preventDefault();
-                    setPlatform(item.id);
-                }}>{item.name}</Dropdown.Item>
-            )
-
-        })
-    }
-    const [categoriesList, setCategoriesList] = useState([]);
-    var known_tmp = [];
-    var tmp = [];
-
-    if (allCategories && adding){
-        handleRender = allCategories;
-        setAdding(false);
-        allCategories.map(item => {
-            setCategoriesList(oldArray => [...oldArray, {"name":item.name,"id":item.id}]);
-        });
-    }
-    if (categoriesList){
-        console.log(typeof toArray(categoriesList))
-    }
 
     if(!props.token || props.role != 'admin')
         return <Forbidden />;
@@ -72,74 +107,128 @@ export const Admin_games = (props) => {
     return (
     <LayoutAdmin>
         <p className="fltr">Dodaj grę</p>
-        <Form className="mb-2">
-            <Row>
-                <Col lg={6} className="mb-2">
-                    <Form.Control type="text" placeholder="Nazwa" onChange={e => setName(e.target.value)}/>
-                </Col>
-                <Col lg={2} className="mb-2">
-                    <Form.Control type="text" placeholder="Cena (zł)" onChange={e => setPrice(e.target.value)}/>
-                </Col>
-                <Col lg={4} className="mb-2">
-                    <Form.Control type="text" placeholder="Ilość wersji pudełkowej" onChange={e => setQuantity(e.target.value)}/>
-                </Col>
-                
-            </Row>
-            <Row>
-                <Col lg={3}>
-                    <DropdownMultiselect buttonClass="mb-2 dropdownmulti-light" placeholder="Kategorie"
-                        options={categoriesList}
-                        name="countries"
-                    />
-                    
-                    <DropdownButton className="mb-2 dropdown-fullw-light" variant="outline-secondary" id="dropdown-basic-button" title="Platforma">
-                        {platformsItems}
+        <Formik 
+                initialValues={{
+                    name: '',
+                    quantity: '',
+                    price: '',
+                    description: '',
+                    age: '',
+                    categories: [],
+                    platform: '',
+                    date: new Date(),
 
-                    </DropdownButton>
-                </Col>
-                <Col lg={3}>
-                    <Form.Control className="mb-2" type="text" placeholder="Wiek" onChange={e => setAge(e.target.value)}/>
-                    <DatePicker
-                        className="calendar mb-2"
-                        onChange={onChange}
-                        value={value}
-                    />
-                </Col>
-               
-                <Col lg={6} className="mb-2" >
-                    <Form.Control as="textarea" style={{ height: "100%"}} rows={2} placeholder="Opis" onChange={e => setDesscr(e.target.value)}/>
-                </Col>
-                
-            </Row>
-            <Row>
-                <Col lg={2} className="offset-lg-10 mb-2">
-                    <Button className="w-100" type="submit" variant="dark" onClick={e => {
-                        e.preventDefault();
+                }}
+                onSubmit={(values,errors) => {  
+                    console.log(values);
+                    setGameAdded(false);
 
-                        var myHeaders = new Headers();
-                        myHeaders.append("Content-Type", "application/json");
-                        myHeaders.append("Authorization", "Bearer " + props.token);
+                    let categories_id = [];
+                    values.categories.map(name => categories_id = categories_id.concat( allCategories.map(cat => cat.name === name ? cat.id : undefined )) );
+                    categories_id = categories_id.filter(x => x !== undefined);
+                    console.log(categories_id);
 
-                        var is_digital = 1;
-                        if (quantity && quantity > 0){
+                    let is_digital = 1;
+                    if (quantity && quantity > 0){
                             is_digital = 0;
-                        }
-                        var raw = JSON.stringify({"name": name, "price" : price, "quantity" : quantity,
-                            "description" : descr, "release_date" : "default", "is_digital" : is_digital,
-                            "platform_id" : platform, "age_category" : age, "categories" : [1]});
+                    }
 
-                        var requestOptions = {
-                            method: 'POST',
-                            headers: myHeaders,
-                            body: raw,
-                            redirect: 'follow'
-                        };
-                        fetch("/addgame", requestOptions)
-                    }}>Dodaj</Button>
-                </Col>
-            </Row>
-            
-        </Form>
+                    fetch("/addgame", {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer " + props.token
+                        },
+                        body: JSON.stringify({"name": values.name, "price" : values.price, "quantity" : values.quantity,
+                        "description" : values.description, "release_date" : "default", "is_digital" : is_digital,
+                        "platform_id" : values.platform, "age_category" : values.age, 
+                        "categories" : categories_id , "release-date": values.date.toString() }),
+                        redirect: 'follow'
+                    }).then(res => { if(res.ok) { setGameAdded(true); updateGames(); } else {errors.setFieldError('err', 'Wystąpił błąd'); } })
+                    .catch(err => console.log(err));
+                }}
+
+                validationSchema={yup.object({
+                    name: yup.string().required('Name is required'),
+                    quantity: yup.number().required('Quantity is required').moreThan(-0.0000000001),
+                    price: yup.number().required('Price is required').moreThan(-0.0000000001),
+                    age: yup.string().required('Age is required'),
+                    categories: yup.array().min(1).required('Categories are required'),
+                    platform: yup.string().required('Platform is required'),
+                    date: yup.date().required('Date is required')
+                })}
+            >
+            {({ handleSubmit, handleChange, values, isValid, errors, touched }) => (
+                <Form className="mb-2" onSubmit={e => e.preventDefault()}>
+                    <Row>
+                        <Col lg={6} className="mb-2">
+                            <Form.Control name="name" type="text" placeholder="Nazwa" value={values.name} onChange={handleChange}
+                                    isInvalid={touched.name && !!errors.name}/>
+                            <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+                        </Col>
+                        <Col lg={2} className="mb-2">
+                            <Form.Control name="price" type="text" placeholder="Cena (zł)" value={values.price} onChange={handleChange}
+                                    isInvalid={touched.price && !!errors.price}/>
+                            <Form.Control.Feedback type="invalid">{errors.price}</Form.Control.Feedback>
+                        </Col>
+                        <Col lg={4} className="mb-2">
+                            <Form.Control name="quantity" type="text" placeholder="Ilość wersji pudełkowej" value={values.quantity} onChange={handleChange}
+                                    isInvalid={touched.quantity && !!errors.quantity}/>
+                            <Form.Control.Feedback type="invalid">{errors.quantity}</Form.Control.Feedback>
+                        </Col>
+                        
+                    </Row>
+                    <Row>
+                        <Col lg={3}>
+                            {allCategories.length === 0 ? null :
+                            <DropdownMultiselectField name="categories" buttonClass="mb-2 dropdownmulti-light" placeholder="Kategorie"
+                                options={allCategories.map(i => i.name)} 
+                            /> }
+                            {touched.categories && errors.categories ? <p className="text-danger" style={{fontSize: "80%"}}>{errors.categories}</p> : null}
+                            
+
+                            <Form.Control name="platform" as="select" className="mr-sm-2 mb-2"
+                                    custom onChange={handleChange} >
+                                    <option value="" >Platforma</option>
+                                    {allPlatforms.map(p => <option value={p.id} >{p.name.toUpperCase()}</option>)}
+                            </Form.Control>
+                            {touched.platform && errors.platform ? <p className="text-danger" style={{fontSize: "80%"}}>{errors.platform}</p> : null}
+                                
+                        </Col>
+                        <Col lg={3}>
+                            <Form.Control name="age" as="select" className="mr-sm-2 mb-2"
+                                    custom onChange={handleChange} >
+                                    <option value="" >Wiek</option>
+                                    <option value="PEGI7">PEGI7</option>
+                                    <option value="PEGI14">PEGI14</option>
+                                    <option value="PEGI16">PEGI16</option>
+                                    <option value="PEGI18">PEGI18</option>
+                            </Form.Control>
+                            {touched.age && errors.age ? <p className="text-danger" style={{fontSize: "80%"}}>{errors.age}</p> : null}
+                            
+
+                            <DatePickerField className="calendar mb-2" name="date" 
+                                value={values.date} />
+                            <Form.Control.Feedback type="invalid">{errors.date}</Form.Control.Feedback>
+                        </Col>
+                    
+                        <Col lg={6} className="mb-2" >
+                            <Form.Control as="textarea" style={{ height: "100%"}} rows={2} name="description" 
+                                placeholder="Opis" value={values.description} onChange={handleChange} />
+                            <Form.Control.Feedback type="invalid">{errors.description}</Form.Control.Feedback>
+                        </Col>
+                        
+                    </Row>
+                    <Row>
+                        <Col lg={2} className="offset-lg-10 mb-2">
+                            <Button className="w-100" type="submit" variant="dark" onClick={handleSubmit}>Dodaj</Button>
+                        </Col>
+                    </Row>
+                    {errors.err ? <p className="text-danger">{errors.err}</p> : null}
+                </Form>
+            )}
+        </Formik>
+        {gameAdded ? <p className="text-success">Gra została pomyślnie dodana</p> : null}
 
         <p className="fltr">Edytuj grę</p>
         <Form className="mb-2">
@@ -159,10 +248,12 @@ export const Admin_games = (props) => {
             </Row>
             <Row>
                 <Col lg={3}>
-                    <DropdownMultiselect buttonClass="mb-2 dropdownmulti-light" placeholder="Kategorie"
-                        options={["Strategiczne", "Przygodowe", "Strzelanki"]}
+                    {allCategories.length === 0 ? null : 
+                    <DropdownMultiselect buttonClass="mb-2 dropdownmulti-light" placeholder="Kategorie" id="multi2"
+                        options={allCategories.map(i => i.name)}
                         name="countries"
                     />
+                    }
                     
                     <DropdownButton className="mb-2 dropdown-fullw-light" variant="outline-secondary" id="dropdown-basic-button" title="Platforma">
                         <Dropdown.Item href="#/action-1">PC</Dropdown.Item>
@@ -200,27 +291,27 @@ export const Admin_games = (props) => {
             <thead>
                 <tr>
                     <th className="no-border-top">Nazwa</th>
-                    <th className="no-border-top">Actions</th>
+                    <th className="no-border-top" style={{ width: "120px" }}>Actions</th>
                 </tr>
             </thead>
             <tbody>
+                {allGames ? allGames.map(g => <tr>
+                    <td>{g.name}</td>
+                    <td>
+                        <Button className="icon p-0 mr-2 pb-1" variant="link"> <BoxArrowUpRight className="text-black-50" size={20} /> </Button>
+                        <Button className="icon p-0 mr-2" variant="link"> <PencilSquare className="text-black-50" size={20} /> </Button>
+                        <Button className="icon p-0" variant="link"> <XCircle className="text-black-50" size={20} /> </Button>
+                    </td>
+                </tr> ) : null}
                 <tr>
                     <td>Wiedźmin 3</td>
-                    <td style={{ width: "120px" }}>
+                    <td>
                         <Button className="icon p-0 mr-2 pb-1" variant="link"> <BoxArrowUpRight className="text-black-50" size={20} /> </Button>
                         <Button className="icon p-0 mr-2" variant="link"> <PencilSquare className="text-black-50" size={20} /> </Button>
                         <Button className="icon p-0" variant="link"> <XCircle className="text-black-50" size={20} /> </Button>
                     </td>
                 </tr>
-                <tr>
-                    <td>Twierdza</td>
-                    <td style={{ width: "120px" }}>
-                        <Button className="icon p-0 mr-2 pb-1" variant="link"> <BoxArrowUpRight className="text-black-50" size={20} /> </Button>
-                        <Button className="icon p-0 mr-2" variant="link"> <PencilSquare className="text-black-50" size={20} /> </Button>
-                        <Button className="icon p-0" variant="link"> <XCircle className="text-black-50" size={20} /> </Button>
-                    </td>
-                </tr>
-                <tr><td colspan="2"></td></tr>
+                <tr><td colSpan="2"></td></tr>
             </tbody>
         </Table>
 
