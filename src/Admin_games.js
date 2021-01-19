da3fa58dac105163795815a74bb4bf78ffc1969c
Matt444 <joniec.mateusz44@gmail.com>
2021-01-19 20:15:57 +0100

    edit games in admin panel

--- src/Admin_games.js
+++ src/Admin_games.js
@@ -40,20 +40,30 @@ const DropdownMultiselectField = ({ ...props }) => {
 
   
 export const Admin_games = (props) => {
-    const [value, onChange] = useState(new Date());
     const [allGames, setAllGames] = useState([]);
+    const [gameId, setGameId] = useState();
     const [name, setName] = useState("");
-    const [price, setPrice] = useState(0);
-    const [quantity, setQuantity] = useState(0);
-    const [descr, setDesscr] = useState("");
+    const [price, setPrice] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [description, setDescription] = useState("");
     const [categories, setCategories] = useState([]);
-    const [age, setAge] = useState();
-    const [platform, setPlatform] = useState();
+    const [age, setAge] = useState("");
+    const [platform, setPlatform] = useState('');
     const [allPlatforms, setAllPlatforms] = useState([]);
     const [allCategories, setAllCategories] = useState([]);
     const [gameAdded, setGameAdded] = useState(false);
-    const [adding, setAdding] = useState(true);
+    const [gameEdited, setGameEdited] = useState(false);
     
+    const handleSetGameEdit = (index) => {
+        setGameId(allGames[index].id);
+        setName(allGames[index].name);
+        setPrice(allGames[index].price);
+        setQuantity(allGames[index].quantity);
+        setDescription(allGames[index].description);
+        setCategories(allGames[index].categories.map(c => c.name));
+        setAge(allGames[index].age_category);
+        setPlatform(allGames[index].platform.id);
+    }
 
     const updateGames = async () => {
         var myHeaders = new Headers();
@@ -133,7 +143,7 @@ export const Admin_games = (props) => {
                             is_digital = 0;
                     }
 
-                    fetch("/addgame", {
+                    fetch("/editgame", {
                         method: 'POST',
                         headers: {
                             "Content-Type": "application/json",
@@ -199,7 +209,13 @@ export const Admin_games = (props) => {
                             <Form.Control name="age" as="select" className="mr-sm-2 mb-2"
                                     custom onChange={handleChange} >
                                     <option value="" >Wiek</option>
+                                    <option value="PEGI3">PEGI3</option>
+                                    <option value="PEGI4">PEGI4</option>
+                                    <option value="PEGI6">PEGI6</option>
                                     <option value="PEGI7">PEGI7</option>
+                                    <option value="PEGI11">PEGI11</option>
+                                    <option value="PEGI12">PEGI12</option>
+                                    <option value="PEGI15">PEGI15</option>
                                     <option value="PEGI14">PEGI14</option>
                                     <option value="PEGI16">PEGI16</option>
                                     <option value="PEGI18">PEGI18</option>
@@ -230,60 +246,139 @@ export const Admin_games = (props) => {
         </Formik>
         {gameAdded ? <p className="text-success">Gra została pomyślnie dodana</p> : null}
 
-        <p className="fltr">Edytuj grę</p>
-        <Form className="mb-2">
-            <Row>
-                <Col lg={6} className="mb-2">
-                    <DropdownButton className="dropdown-fullw-light" variant="outline-secondary" id="dropdown-basic-button" title="Nazwa">
-                        <Dropdown.Item href="#/action-1">Twierdza</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-                <Col lg={2} className="mb-2">
-                    <Form.Control type="text" placeholder="Cena (zł)" />
-                </Col>
-                <Col lg={4} className="mb-2">
-                    <Form.Control type="text" placeholder="Ilość wersji pudełkowej" />
-                </Col>
-                
-            </Row>
-            <Row>
-                <Col lg={3}>
-                    {allCategories.length === 0 ? null : 
-                    <DropdownMultiselect buttonClass="mb-2 dropdownmulti-light" placeholder="Kategorie" id="multi2"
-                        options={allCategories.map(i => i.name)}
-                        name="countries"
-                    />
+        <p className="fltr" id="edit">Edytuj grę</p>
+        <Formik 
+                initialValues={{
+                    name: '',
+                    quantity: '',
+                    price: '',
+                    description: '',
+                    age: '',
+                    categories: [],
+                    platform: '',
+                    date: new Date(),
+
+                }}
+                onSubmit={(values,errors) => {  
+                    console.log(values);
+                    setGameEdited(false);
+
+
+                    let categories_id = [];
+                    values.categories.map(name => categories_id = categories_id.concat( allCategories.map(cat => cat.name === name ? cat.id : undefined )) );
+                    categories_id = categories_id.filter(x => x !== undefined);
+                    console.log(categories_id);
+
+                    let is_digital = 1;
+                    if (values.quantity && values.quantity > 0){
+                            is_digital = 0;
                     }
+
+                    fetch("/editgame", {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer " + props.token
+                        },
+                        body: JSON.stringify({"game_id": gameId ,"name": name, "price" : price, "quantity" : values.quantity,
+                        "description" : description, "release_date" : "default", "is_digital" : is_digital,
+                        "platform_id" : platform, "age_category" : values.age, 
+                        "categories" : categories_id , "release-date": values.date.toString() }),
+                        redirect: 'follow'
+                    }).then(res => { if(res.ok) { setGameEdited(true); updateGames(); } else errors.setFieldError('err','Wystąpił błąd') })
+                    .catch(err => console.log(err));
+
                     
-                    <DropdownButton className="mb-2 dropdown-fullw-light" variant="outline-secondary" id="dropdown-basic-button" title="Platforma">
-                        <Dropdown.Item href="#/action-1">PC</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">PS4</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-                <Col lg={3}>
-                    <Form.Control className="mb-2" type="text" placeholder="Wiek" />
-                    <DatePicker
-                        className="calendar mb-2"
-                        onChange={onChange}
-                        value={value}
-                    />
-                </Col>
-               
-                <Col lg={6} className="mb-2" >
-                    <Form.Control as="textarea" style={{ height: "100%"}} rows={2} placeholder="Opis" />
-                </Col>
-                
-            </Row>
-            <Row>
-                <Col lg={3} className="offset-lg-7 mb-2">
-                    <Button className="w-100" type="submit" variant="outline-dark">Dodaj klucze</Button>
-                </Col>
-                <Col lg={2} className="mb-2">
-                    <Button className="w-100" type="submit" variant="dark">Zastosuj</Button>
-                </Col>
-            </Row>
-            
-        </Form>
+                }}
+
+                validationSchema={yup.object({
+                    name: yup.string().required('Choose game...'),
+                    quantity: yup.number().moreThan(-0.0000000001),
+                    price: yup.number().moreThan(-0.0000000001),
+                    age: yup.string(),
+                    categories: yup.array(),
+                    platform: yup.string(),
+                    date: yup.date().required('Date is required')
+                })}
+            >
+            {({ handleSubmit, handleChange, values, isValid, errors, touched }) => (
+                <Form className="mb-2" onSubmit={e => e.preventDefault()}>
+                    <Row>
+                        <Col lg={6} className="mb-2">
+                            <DropdownButton name="name" className="dropdown-fullw-light" variant="outline-secondary" title={name === '' ? "Wybierz grę..." : name}>
+                                {allGames ? allGames.map((g,index) => <Dropdown.Item onClick={() => 
+                                    { handleSetGameEdit(index); values.name=g.name; }}>{g.name}</Dropdown.Item>) : null}
+                            </DropdownButton>
+                        </Col>
+                        <Col lg={2} className="mb-2">
+                            <Form.Control name="price" type="text" placeholder="Cena (zł)" value={price} onChange={(e) => {setPrice(e.target.value); values.price = e.target.value;}}
+                                    isInvalid={touched.price && !!errors.price}/>
+                            <Form.Control.Feedback type="invalid">{errors.price}</Form.Control.Feedback>
+                        </Col>
+                        <Col lg={4} className="mb-2">
+                            <Form.Control name="quantity" type="text" placeholder="Ilość wersji pudełkowej" value={quantity} onChange={(e) => { setQuantity(e.target.value); values.quantity = e.target.value;}}
+                                    isInvalid={touched.quantity && !!errors.quantity}/>
+                            <Form.Control.Feedback type="invalid">{errors.quantity}</Form.Control.Feedback>
+                        </Col>
+                        
+                    </Row>
+                    <Row>
+                        <Col lg={3}>
+                            {allCategories.length === 0 ? null :
+                            <DropdownMultiselectField name="categories" buttonClass="mb-2 dropdownmulti-light" placeholder="Kategorie"
+                                options={allCategories.map(i => i.name) } selected={categories} handleOnChange={s => setCategories(s)}
+                            /> }
+                            {touched.categories && errors.categories ? <p className="text-danger" style={{fontSize: "80%"}}>{errors.categories}</p> : null}
+                            
+
+                            <Form.Control name="platform" as="select" className="mr-sm-2 mb-2"
+                                    custom onChange={(e) => { setPlatform(e.target.value); values.platform = e.target.value;}} value={platform}>
+                                    <option value="" >Platforma</option>
+                                    {allPlatforms.map(p => <option value={p.id} >{p.name.toUpperCase()}</option>)}
+                            </Form.Control>
+                            {touched.platform && errors.platform ? <p className="text-danger" style={{fontSize: "80%"}}>{errors.platform}</p> : null}
+                                
+                        </Col>
+                        <Col lg={3}>
+                            <Form.Control name="age" as="select" className="mr-sm-2 mb-2" value={age}
+                                    custom onChange={handleChange} >
+                                    <option value="" >Wiek</option>
+                                    <option value="PEGI3">PEGI3</option>
+                                    <option value="PEGI4">PEGI4</option>
+                                    <option value="PEGI6">PEGI6</option>
+                                    <option value="PEGI7">PEGI7</option>
+                                    <option value="PEGI11">PEGI11</option>
+                                    <option value="PEGI12">PEGI12</option>
+                                    <option value="PEGI15">PEGI15</option>
+                                    <option value="PEGI14">PEGI14</option>
+                                    <option value="PEGI16">PEGI16</option>
+                                    <option value="PEGI18">PEGI18</option>
+                            </Form.Control>
+                            {touched.age && errors.age ? <p className="text-danger" style={{fontSize: "80%"}}>{errors.age}</p> : null}
+                            
+
+                            <DatePickerField className="calendar mb-2" name="date" 
+                                value={values.date} />
+                            <Form.Control.Feedback type="invalid">{errors.date}</Form.Control.Feedback>
+                        </Col>
+                    
+                        <Col lg={6} className="mb-2" >
+                            <Form.Control as="textarea" style={{ height: "100%"}} rows={2} name="description" 
+                                placeholder="Opis" value={description} onChange={handleChange} />
+                            <Form.Control.Feedback type="invalid">{errors.description}</Form.Control.Feedback>
+                        </Col>
+                        
+                    </Row>
+                    <Row>
+                        <Col lg={2} className="offset-lg-10 mb-2">
+                            <Button className="w-100" type="submit" variant="dark" onClick={handleSubmit}>Zastosuj</Button>
+                        </Col>
+                    </Row>
+                    {errors.err ? <p className="text-danger">{errors.err}</p> : null}
+                </Form>
+            )}
+        </Formik>
+        {gameEdited ? <p className="text-success">Gra została pomyślnie zmodyfikowana</p> : null}
 
         <p className="fltr">Wszystkie gry</p>
 
@@ -295,22 +390,14 @@ export const Admin_games = (props) => {
                 </tr>
             </thead>
             <tbody>
-                {allGames ? allGames.map(g => <tr>
+                {allGames ? allGames.map((g,index) => <tr>
                     <td>{g.name}</td>
                     <td>
-                        <Button className="icon p-0 mr-2 pb-1" variant="link"> <BoxArrowUpRight className="text-black-50" size={20} /> </Button>
-                        <Button className="icon p-0 mr-2" variant="link"> <PencilSquare className="text-black-50" size={20} /> </Button>
-                        <Button className="icon p-0" variant="link"> <XCircle className="text-black-50" size={20} /> </Button>
+                        {/* <Button className="icon p-0 mr-2 pb-1" variant="link"> <BoxArrowUpRight className="text-black-50" size={20} /> </Button> */}
+                        <Button className="icon p-0 mr-2" href="#edit" variant="link"> <PencilSquare className="text-black-50" size={20} onClick={() => handleSetGameEdit(index)} /> </Button>
+                        {/* <Button className="icon p-0" variant="link"> <XCircle className="text-black-50" size={20} /> </Button> */}
                     </td>
                 </tr> ) : null}
-                <tr>
-                    <td>Wiedźmin 3</td>
-                    <td>
-                        <Button className="icon p-0 mr-2 pb-1" variant="link"> <BoxArrowUpRight className="text-black-50" size={20} /> </Button>
-                        <Button className="icon p-0 mr-2" variant="link"> <PencilSquare className="text-black-50" size={20} /> </Button>
-                        <Button className="icon p-0" variant="link"> <XCircle className="text-black-50" size={20} /> </Button>
-                    </td>
-                </tr>
                 <tr><td colSpan="2"></td></tr>
             </tbody>
         </Table>
